2ced3a73fa8a7b2a6e20619503ad4ad5814dc4e9
suheon.lim <suheon.lim@lemonhc.com>
2022-10-21 17:23:33 +0900

    VanillaJS - 문벅스 시작

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,6 @@
 
 // -- CREATE --
-// []  메뉴의 이름을 입력받고 확인버튼을 누르면 추가된다.
-// []  메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다.
+// []  메뉴의 이름을 입력받고 확인버튼 또는 엔터를 누르면 추가된다.
 // []  추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
 // []  총 메뉴 갯수를 count하여 상단에 보여준다.
 // []  메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
@@ -19,16 +18,10 @@
 // []  확인 버튼을 누르면 메뉴를 삭제된다.
 // []  총 메뉴 갯수를 count하여 상단에 보여준다.
 
-function App() {
-    // form 태그 이벤트 막기
+const $ = (selectors) => document.querySelector(selectors);
 
-    // 메뉴의 이름을 입력받기
-    document.querySelector("#espresso-menu-name").addEventListener("keypress",(e) => {
-        console.log(e.key);
-        if(e.key === 'Enter'){
-            console.log(document.querySelector("espresso-menu-name").value);
-        }
-    });
+function App() {
+
 }
 
 App();
