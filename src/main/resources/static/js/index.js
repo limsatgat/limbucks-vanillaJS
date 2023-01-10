0240c221cfb76d66f5a83180300473527e5bbc4b
limsatgat <96382179+limsatgat@users.noreply.github.com>
2023-01-10 21:59:58 +0900

    문벅스 스텝1 메뉴판만들기 끝

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,19 @@
 const $ = (selectors) => document.querySelector(selectors);
 
 function App() {
-// -- CREATE --
+    // CRUD
     const emptyCheck = () => {
         if ($('#espresso-menu-name').value === '') {
             alert('추가할 메뉴를 입력해주세요.');
             return;
         }
     }
+
+    const menuCnt = () => {
+        const menuCnt = $('#espresso-menu-list').querySelectorAll('li').length;
+        $('.menu-count').innerText = `총 ${menuCnt}개`;
+    }
+
     const updateMenu = () => {
         const coffeeName = $('#espresso-menu-name').value;
         const addCoffee = (coffeeName) => {
@@ -17,15 +23,12 @@ function App() {
                 <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">삭제</button></li>`
         };
         $('#espresso-menu-list').insertAdjacentHTML('beforeend', addCoffee(coffeeName));
-        // []  메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
         $('#espresso-menu-name').value = '';
-        // []  총 메뉴 갯수를 count하여 상단에 보여준다.
-        const menuCnt = $('#espresso-menu-list').querySelectorAll('li').length;
-        $('.menu-count').innerText = `총 ${menuCnt}개`;
+        menuCnt();
     }
     $('#espresso-menu-form').addEventListener('submit', (e) => {
         e.preventDefault();
-    })
+    });
     $('#espresso-menu-name').addEventListener('keypress', (e) => {
         if ($('#espresso-menu-name').value === '') {
             alert('추가할 메뉴를 입력해주세요.');
@@ -33,30 +36,42 @@ function App() {
         }
         if (e.key === 'Enter'){
             updateMenu();
+            menuCnt();
         };
-    })
+    });
     $('#espresso-menu-submit-button').addEventListener('click', () => {
         if ($('#espresso-menu-name').value === '') {
             alert('추가할 메뉴를 입력해주세요.');
             return;
         }
         updateMenu();
-    })
+        menuCnt();
+    });
 
-// -- UPDATE --
-// []  메뉴의 수정 버튼을 누르면 이벤트가 발생하여, 메뉴 수정하는 모달창이 출력
-    $('#menu-edit-button').addEventListener('click', (e) => {
+    $('#espresso-menu-list').addEventListener('click', (e) => {
+        if(e.target.classList.contains('menu-edit-button')){
+            const changedTarget = e.target.closest('li').querySelector('.menu-name');
+            const newCoffee = prompt('변경할 메뉴명을 입력하시오', changedTarget.innerText);
+            changedTarget.innerText = newCoffee;
+        }
+        if(e.target.classList.contains('menu-remove-button')){
+            if(confirm('삭제하시겠습니까?')){
+                e.target.closest('li').remove();
+                menuCnt();
+            }
+        }
+    });
+    // -- CRUD END
 
-    })
-// []  모달창에서 신규 메뉴명을 입력받고, 확인을 누르면 수정이 된다.
-// []  메뉴 수정시 브라우저에서 제공하는 prompt 인터페이스를 활용한다.
-// []  총 메뉴 갯수를 count하여 상단에 보여준다.
+    // 상태관리
+    // [] - localStorage에 데이터를 저장한다.
+    // [] - localStoragfe에 있는 데이터를 읽어온다.
 
-// -- DELETE --
-// []  메뉴 삭제 버튼을 이벤트가 발생하고,
-// []  메뉴 삭제시 브라우저에서 제공하는 confirm 인터페이스를 활용한다.
-// []  확인 버튼을 누르면 메뉴를 삭제된다.
-// []  총 메뉴 갯수를 count하여 상단에 보여준다.
+    // [] - 에스프레소, 프라푸치노, 블렌디드, 티바나, 디저트 각각의 종류별로 메뉴판을 관리할 수 있게 만든다.
+    // [] - 페이지에 최초로 접근할 때는 에스프레소 메뉴가 먼저 보이게 한다.
+    // [] - 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다
 }
+
+
 
 App();
